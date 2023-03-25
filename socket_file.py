77f45a124dbbf06995d43340e303d8917d676f1d
Theblockhead_ <heheheeheheh10@gmail.com>
2023-03-25 17:37:20 +0530

    adding socket_file

---
+++ socket_file.py
@@ -0,0 +1,13 @@
+import socket
+
+serverIP = '172.17.29.11'
+serverPORT = 6000
+serveraddress = (serverIP, serverPORT)
+bufferSize = 1024
+UDPClientSocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
+
+message = 'Hi, my name is Varshith '
+
+bytestosend = str.encode(message)
+
+UDPClientSocket.sendto(bytestosend, serveraddress)
